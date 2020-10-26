297e63ea2b9bed1e31089923461a5868ed59cf80
Bew <Bew@sysops-MBP.broadband>
2020-10-26 20:03:50 +0000

    update axios api

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({ //customize 
     baseURL:'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID yuX3mNH0s_NvZNP3qfZGGh1WvIyGiwl2FST739In8OU'
+        Authorization: 'Client-ID YOUR_KEY'
     }
 });
 
