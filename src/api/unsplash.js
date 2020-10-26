0372d0aca98c0a4914333aa58b638bd00f55815f
Juti <bew.emc2@gmail.com>
2020-10-26 20:02:43 +0000

    Delete unsplash.js
    will update later

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({ //customize 
-    baseURL:'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID yuX3mNH0s_NvZNP3qfZGGh1WvIyGiwl2FST739In8OU'
-    }
-});
-
